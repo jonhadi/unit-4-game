0c0589717b7c8c6c71a284be68abb2d50fa46e24
JonHadi <hadijonathan@gmail.com>
2019-05-03 22:06:50 -0700

    made the picking function on hero and enemy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,42 @@
+var charList = {
+    char1 : {
+        name: "Mushroom",
+        HP: 100,
+        AP: 10,
+        AG: 10,
+        CAP: 5
+    },
 
+    char2 : {
+        name: "Whale",
+        HP: 250,
+        AP: 2,
+        AG: 2,
+        CAP: 1
+    },
 
-var character1 = {
-    name: "Mushroom",
-    HP: 100,
-    AP: 10,
-    AG: 10,
-    CAP: 5
-};
+    char3 : {
+        name: "Archer",
+        HP: 50,
+        AP: 20,
+        AG: 20,
+        CAP: 15
+    },
 
-var character2 = {
-    name: "Whale",
-    HP: 250,
-    AP: 2,
-    AG: 2,
-    CAP: 1
-};
+    char4 : {
+        name: "Dragon",
+        HP: 25,
+        AP: 25,
+        AG: 20,
+        CAP: 20
+    }
 
-var character3 = {
-    name: "Archer",
-    HP: 50,
-    AP: 20,
-    AG: 20,
-    CAP: 15
-};
-
-var character4 = {
-    name: "Dragon",
-    HP: 25,
-    AP: 25,
-    AG: 20,
-    CAP: 20
 };
 
 //game
 
 //show characters
-gameStart();
+
 //pick character
 
 //pick enemy
@@ -45,26 +46,41 @@ gameStart();
 //pick next enemy
 
 //repeat till win or lose
-var champ = character1;
-var chump = character2;
+var champ;
+var chump;
+// = {...charList[char2]};
+//console.log({...charList[char2]});
+console.log(champ);
 
-function gameStart() {
-    $("char1").txt(character1.name)
-}
+$(".hero").on("click", function(me) {
+    console.log(me);
+    console.log(me.target.id);
+    if (champ === undefined) {
+        champ = {...charList[me.target.id]};
+        console.log("champ");
+        console.log(champ);
+    } else {
+        console.log("chump");
+        chump = {...charList[me.target.id]};
+        console.log(chump);
+    }
+});
+
 
 function pickChar() {
 
 }
 
 function attack(champ, chump) {
+    console.log(champ);
     $("#arena").empty();
     chump.HP -= champ.AP;
     console.log("Chump HP: " + chump.HP)
-    $("#arena").append("You attacked " + chump.name + " for " + champ.AP + " damage.");
+    $("#champAtk").html("You attacked " + chump.name + " for " + champ.AP + " damage.");
     champ.AP += champ.AG;
-    console.log("New AP: " + champ.AP)
+    console.log("New AP: " + champ.AP);
     champ.HP -= chump.CAP;
     console.log("Champ HP: " + champ.HP);
-    $("#arena").append("\n" + chump.name + " attacked " + " back for " + chump.CAP + " damage.");
+    $("#chumpAtk").html(chump.name + " attacked " + champ.name + " back for " + chump.CAP + " damage.");
 
 }
