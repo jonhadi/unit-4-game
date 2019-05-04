9a1317990d5d63b11d1994014268dfdf2a5e04da
JonHadi <hadijonathan@gmail.com>
2019-05-04 16:03:55 -0700

    able to move to game state, pick character, move enemy line, pick enemy, lower hp per attack

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,8 +10,8 @@ var charList = {
     char2 : {
         name: "Whale",
         HP: 250,
-        AP: 2,
-        AG: 2,
+        AP: 5,
+        AG: 5,
         CAP: 1
     },
 
@@ -27,11 +27,20 @@ var charList = {
         name: "Dragon",
         HP: 25,
         AP: 25,
-        AG: 20,
+        AG: 25,
         CAP: 20
     }
 
 };
+//character to input through jquery if time
+var char5 = {
+    name: "Knight",
+    HP: 500,
+    AP: 500,
+    CAP: 0,
+    AG: -50,
+    profile: "../images/Knight.jpg"
+}
 
 //game
 
@@ -56,13 +65,28 @@ $(".hero").on("click", function(me) {
     console.log(me);
     console.log(me.target.id);
     if (champ === undefined) {
+        $('#intro').attr('class', 'hide-me');
+        //if no champ set champ to id of click
         champ = {...charList[me.target.id]};
         console.log("champ");
-        console.log(champ);
+        console.log(me.target.parentElement.id);
+        //removes hero class so it cant be clicked, doesnt work though
+        //$('#'+me.target.id).removeAttr('class');
+        //removes charCard and makes champ card so background doesnt turn red
+        $("#"+me.target.parentElement.id).removeAttr('class').attr('id', 'champCard').detach().prependTo('#your-char');
+        //move character list to enemies area
+        $('#character-list').detach().appendTo('#enemiesATA');
+        $('.charCard').attr('class', 'enemy');
     } else {
         console.log("chump");
-        chump = {...charList[me.target.id]};
-        console.log(chump);
+        if (chump === undefined) {
+            chump = {...charList[me.target.id]};
+            console.log(chump);
+            $("#"+me.target.parentElement.id).removeAttr('class').attr('id', 'chumpCard').detach().appendTo('#chump');
+            //change color of text to white for black background
+            $("#"+me.target.previousElementSibling.id).attr('class', 'enemy-text');
+            $("#"+me.target.nextElementSibling.id).attr('class', 'enemy-text');
+        }
     }
 });
 
@@ -72,10 +96,12 @@ function pickChar() {
 }
 
 function attack(champ, chump) {
-    console.log(champ);
+    console.log();
     $("#arena").empty();
     chump.HP -= champ.AP;
-    console.log("Chump HP: " + chump.HP)
+    console.log("Chump HP: " + chump.HP);
+    var hpID = '#' + $('#chumpCard').children()[2].id;
+    $(hpID).html(chump.HP);
     $("#champAtk").html("You attacked " + chump.name + " for " + champ.AP + " damage.");
     champ.AP += champ.AG;
     console.log("New AP: " + champ.AP);
